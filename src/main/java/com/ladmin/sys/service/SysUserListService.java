779f2c1c26d569c134de4a7c64545313cb75938e
qiao970515 <65222062+qiao970515@users.noreply.github.com>
2020-05-21 09:28:46 +0800

    9:24
    list

---
+++ src/main/java/com/ladmin/sys/service/SysUserListService.java
@@ -0,0 +1,13 @@
+package com.ladmin.sys.service;
+
+import java.util.List;
+import java.util.Map;
+
+import com.ladmin.sys.entity.SysUserList;
+import com.ladmin.sys.vo.SysUserListVo;
+
+public interface SysUserListService {
+	
+	List<SysUserList> findObjects();
+	
+}
