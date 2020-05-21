899cdc98da26902eada6bba9770285e235c4b322
qiao970515 <65222062+qiao970515@users.noreply.github.com>
2020-05-21 09:17:57 +0800

    list
    list

---
+++ src/main/java/com/ladmin/sys/controller/SysUserListController.java
@@ -0,0 +1,35 @@
+package com.ladmin.sys.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.ladmin.common.vo.JsonResult;
+import com.ladmin.sys.entity.SysUserList;
+import com.ladmin.sys.service.SysUserListService;
+import com.ladmin.sys.vo.SysUserListVo;
+/**
+ * qiao
+ * @author Dell
+ */
+@Controller
+@RequestMapping("/user")
+public class SysUserListController {
+	
+	@Autowired
+	private SysUserListService sysUserListService;
+	
+	@RequestMapping("/doFindActivitys")
+	public String doFindActivitys(Model model) {
+		List<SysUserList> list=sysUserListService.findObjects();
+		System.out.println("list="+list);
+		model.addAttribute("list", list);
+		//模拟耗时操作
+		
+		return null;
+	} 
+}
